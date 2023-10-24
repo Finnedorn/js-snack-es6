3df2ea3d1cb6fad5fbae117191383f34aebd247d
Finnedorn <daniele.giuntoli@outlook.com>
2023-10-24 16:19:25 +0200

    js: added snack bock pt1 and pt2

--- js/script.js
+++ js/script.js
@@ -11,5 +11,104 @@ Generiamo e stampiamo in console la lista per i segnaposto.
 */
 
 
+const VipTable = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga', 
+    'Cristiano Ronaldo', 
+    'Georgina Rodriguez', 
+    'Chiara Ferragni', 
+    'Fedez', 
+    'George Clooney', 
+    'Amal Clooney', 
+    'Maneskin'
+];
+
+const table = VipTable.map((element, index) => {
+    let i = {
+        guestname: element,
+        tablename: index + 1
+    };
+    return i;
+});
+console.log(table);
+
+
+
+/*
+SNACK 2
+Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
+ES (Marco della Rovere => MARCO DELLA ROVERE);
+2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+
+Questo è l'elenco degli studenti:
+Id  Name                Grades
+213 Marco della Rovere      78
+110 Paola Cortellessa       96
+250 Andrea Mantegna         48
+145 Gaia Borromini          74
+196 Luigi Grimaldello       68
+102 Piero della Francesca   50
+120 Francesca da Polenta    84
+*/
+
+const students = [
+    {
+        id: 213,
+        name: 'Marco della Rovere',
+        grades: 78 
+    },
+    {
+        id: 110,
+        name: 'Paola Cortellessa',
+        grades: 96 
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        grades: 48
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        grades: 74
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grades: 68
+    },
+    {
+        id: 102,
+        name: 'Piero della Francesca',
+        grades: 50 
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        grades: 84
+    },
+];
+
+const namesStudents = students.filter((el) => {
+    return el.name.toUpperCase(); 
+});
+console.log(namesStudents);
+
+
+const gradesStudents = students.filter((el) => {
+    return el.grades > 70;
+});
+console.log(gradesStudents);
+
+const idsStudents = students.filter((el) => {
+    return el.grades > 70 && el.id > 120;
+});
+console.log(idsStudents);
+
+
+
 
 
